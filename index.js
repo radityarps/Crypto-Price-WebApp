646b4013cdc6760e09db0b0cc1a88393bd9ac220
radityarps <radityarafifps@gmail.com>
2023-11-11 18:50:13 +0700

    create get request for API

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import "dotenv/config";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 const app = express();
+const apiKey = process.env.apiKey;
+const configHeader = {
+  headers: { Authorization: `Bearer ${apiKey}` },
+};
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  try {
+    const listCoins = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/list"
+    );
+    res.render("index.ejs", {
+      data: listCoins.data[1].id,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
